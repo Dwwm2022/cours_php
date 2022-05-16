859fa9aeaeda19e4329aa841b669d072d10e7932
worou <adimicool@gmail.com>
2022-05-16 14:18:23 +0200

    add mehodes

--- poo/exemples/Person.class.php
+++ poo/exemples/Person.class.php
@@ -4,5 +4,17 @@ class Person{
 
     public $nom = "";
     public $age = 0;
+
+    public function affichage():string{
+        return "Nom: ".$this->nom.", Age: ".$this->age."ans";
+    }
+
+    public function isMineur():bool{
+        if($this->age >= 18){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 
--- poo/exemples/main_person.php
+++ poo/exemples/main_person.php
@@ -3,15 +3,30 @@
 require_once('Person.class.php');
 
 $pers1  = new Person();//objet pers1 ou instance pers1
-$pers1->nom = "Thomas";
+$pers1->nom = "Thomas";//assignation
 $pers1->age = 52;
 
 $pers2 = new Person();
 $pers2->nom = "Dupond";
 $pers2->age = 21;
-echo "Nom: ".$pers1->nom. " Age: ".$pers1->age."ans";
+//echo "Nom: ".$pers1->nom. " Age: ".$pers1->age."ans";
+echo $pers1->affichage();
 echo "<br/>";
-echo "Nom: ".$pers2->nom. " Age: ".$pers2->age."ans";
+echo $pers2->affichage();
+//echo "Nom: ".$pers2->nom. " Age: ".$pers2->age."ans";
 
-$person_tab = [$pers1,$pers2];
+$person_tab = [$pers1, $pers2];
 //var_dump($pers1);
+
+echo"<ol>
+<li>Thomas</li>
+<li>Dupond</li>
+</ol>";
+
+echo"<ol>";
+foreach ($person_tab as $person) {
+   echo "<li>".$person->nom."</li>";
+}
+echo"</ol>";
+
+echo $pers1->age ? "Vous êtes majeur" : "Vous êtes mineur";
