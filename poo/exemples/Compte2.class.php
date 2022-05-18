43b976a483dfab5d9f2ded3b358fdf14ddf6282a
Pehounde Nicodeme <59A3066@afpa.fr>
2022-05-18 16:27:10 +0200

    add interface

--- poo/exemples/Compte2.class.php
+++ poo/exemples/Compte2.class.php
@@ -1,68 +1,73 @@
 <?php
+    abstract class Compte2{
+        
+        protected string $titulaire = "";
+        protected string $numero = "";
+        protected float $solde = 0.0;
 
-class Compte2{
-    protected string $titulaire = "";
-    protected string $numero = "";
-    protected float $solde = 0.0;
+        public function __construcct($t,$n,$s){
+            $this->titulaire = $t;
+            $this->numero = $n;
+            $this->solde = $s;
+        }
 
-    public function __construct($t, $n, $s){
-        $this->titulaire = $t;
-        $this->numero = $n;
-        $this->numero = $s;
+        /**
+         * Get the value of titulaire
+         */ 
+        public function getTitulaire()
+        {
+                return $this->titulaire;
+        }
+
+        /**
+         * Set the value of titulaire
+         *
+         * @return  self
+         */ 
+        public function setTitulaire($titulaire)
+        {
+                $this->titulaire = $titulaire;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of numero
+         */ 
+        public function getNumero()
+        {
+                return $this->numero;
+        }
+
+        /**
+         * Set the value of numero
+         *
+         * @return  self
+         */ 
+        public function setNumero($numero)
+        {
+                $this->numero = $numero;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of solde
+         */ 
+        public function getSolde()
+        {
+                return $this->solde;
+        }
+
+        public function depot(float $montant){
+            $this->solde += $montant;
+        }
+
+        public function affichage(){
+                
+            return "Numero: ".$this->numero." Titulaire: ".$this->titulaire." Solde: ".$this->solde.".<br/>";
+        }
+
+        abstract function retrait(float $montant);
     }
-
-    /**
-     * Get the value of titulaire
-     */ 
-    public function getTitulaire()
-    {
-        return $this->titulaire;
-    }
-
-    /**
-     * Set the value of titulaire
-     *
-     * @return  self
-     */ 
-    public function setTitulaire($titulaire)
-    {
-        $this->titulaire = $titulaire;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of numero
-     */ 
-    public function getNumero()
-    {
-        return $this->numero;
-    }
-
-    /**
-     * Set the value of numero
-     *
-     * @return  self
-     */ 
-    public function setNumero($numero)
-    {
-        $this->numero = $numero;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of solde
-     */ 
-    public function getSolde()
-    {
-        return $this->solde;
-    }
-
-    public function depot($montant){
-        $this->solde += $montant;
-    }
-    public function affichage(){
-        return "Identifiant: ".$th
-    }
-}
+?>
