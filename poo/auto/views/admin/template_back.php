56f51303874fc6e2fb1654a0f0e8ec2b1646da97
Nicodeme <Nicodeme.Pehounde@externe.apfa.fr>
2022-05-23 16:46:03 +0200

    add template

---
+++ poo/auto/views/admin/template_back.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../../assets/css/style.css">
+</head>
+<body>
+
+<div class="sidenav">
+    <h1 class="text-white">Auto</h1>
+  <a href="#about"></a>
+  
+  <button class="dropdown-btn">Catégories
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-container">
+    <a href="#">Ajouter</a>
+    <a href="#">Lister</a>
+  </div>
+  <button class="dropdown-btn">Véhicule
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-container">
+    <a href="#">Ajouter</a>
+    <a href="#">Lister</a>
+  </div>
+  <a href="#contact">Search</a>
+</div>
+
+<div class="main">
+  <h2>Sidebar Dropdown</h2>
+  <p>Click on the dropdown button to open the dropdown menu inside the side navigation.</p>
+  <p>This sidebar is of full height (100%) and always shown.</p>
+  <p>Some random text..</p>
+</div>
+
+<script src="../../assets/js/script.js"></script>
+
+</body>
+</html> 
