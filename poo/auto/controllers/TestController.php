b743dac95f5dcdc8f9837f8a631f42df39ef6a3e
Nicodeme <Nicodeme.Pehounde@externe.apfa.fr>
2022-05-23 14:23:32 +0200

    use absolute path

--- poo/auto/controllers/TestController.php
+++ poo/auto/controllers/TestController.php
@@ -1,2 +1,20 @@
 <?php
 
+//die(dirname(__DIR__).'/models/TestModel.php');
+require_once(dirname(__DIR__).'/models/TestModel.php');
+class TestController{
+
+    private $test_model;
+
+    public function __construct(){
+        $this->test_model = new TestModel();
+    }
+
+    public function listData(){
+        $data = $this->test_model->getData();
+        require_once(dirname(__DIR__).'/views/testView.php');
+    }
+}
+
+$tctr = new TestController();
+$tctr->listData();
