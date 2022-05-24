96e5deb6d5197469a3e642f69972bface71b5728
Nicodeme <Nicodeme.Pehounde@externe.apfa.fr>
2022-05-24 13:38:47 +0200

    add template

--- poo/auto/views/admin/categories/listView.php
+++ poo/auto/views/admin/categories/listView.php
@@ -1,3 +1,7 @@
 <?php
+ob_start();
+//var_dump($categories);
+echo"Hello world!";
+$content = ob_get_clean();
+require_once(dirname(dirname(__DIR__)).'/admin/template_back.php');
 
-var_dump($categories);
